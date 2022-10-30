15ec6ac2ed837691e43a42f550f2170b56a1a4e6
Llydo1 <beaniss1357@gmail.com>
2022-10-30 10:12:40 +0700

    Add features?

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,19 +1,19 @@
 const networkConfig = {
-  1: {
-    name: "Ethereum mainnet",
-    ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-    explorer_url: "https://etherscan.io/",
-  },
-  5: {
-    name: "Goerli",
-    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
-    explorer_url: "https://goerli.etherscan.io/",
-  },
-  56: {
-    name: "Binance smart chain",
-    ethUsdPriceFeed: "0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e",
-    explorer_url: "https://bscscan.com/",
-  },
+    1: {
+        name: "Ethereum mainnet",
+        ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+        explorer_url: "https://etherscan.io/",
+    },
+    5: {
+        name: "Goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+        explorer_url: "https://goerli.etherscan.io/",
+    },
+    56: {
+        name: "Binance smart chain",
+        ethUsdPriceFeed: "0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e",
+        explorer_url: "https://bscscan.com/",
+    },
 };
 
 const developmentChains = ["hardhat", "localhost"];
@@ -21,8 +21,8 @@ const DECIMALS = 8;
 const INITIAL_ANSWER = 150000000000;
 
 module.exports = {
-  networkConfig,
-  developmentChains,
-  DECIMALS,
-  INITIAL_ANSWER,
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
 };
